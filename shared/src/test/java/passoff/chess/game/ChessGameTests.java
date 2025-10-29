8aebb445ccb7b43317ab5856513d0163d14b2aa4
Melvin Whitaker <awsome7mailman@gmail.com>
2025-10-28 20:06:50 -0600

    Added extra clarification on ChessGameTests

--- shared/src/test/java/passoff/chess/game/ChessGameTests.java
+++ shared/src/test/java/passoff/chess/game/ChessGameTests.java
@@ -19,7 +19,7 @@ public class ChessGameTests extends EqualsTestingUtility<ChessGame> {
 
     @Override
     protected Map.Entry<String, ChessGame> buildOriginal() {
-        return Map.entry("Default board", new ChessGame());
+        return Map.entry("Chess Game with Default board", new ChessGame());
     }
 
     @Override
@@ -29,21 +29,21 @@ public class ChessGameTests extends EqualsTestingUtility<ChessGame> {
         try {
             ChessGame game1 = new ChessGame();
             game1.setTeamTurn(ChessGame.TeamColor.BLACK);
-            differentGames.put("Different team turn", game1);
+            differentGames.put("Chess Game with Different team turn", game1);
 
             ChessGame game2 = new ChessGame();
             game2.makeMove(new ChessMove(
                     new ChessPosition(2, 5),
                     new ChessPosition(4, 5),
                     null));
-            differentGames.put("Moved pawn", game2);
+            differentGames.put("Chess Game with Moved pawn", game2);
 
             ChessGame game3 = new ChessGame();
             game3.makeMove(new ChessMove(
                     new ChessPosition(1, 7),
                     new ChessPosition(3, 6),
                     null));
-            differentGames.put("Moved knight", game3);
+            differentGames.put("Chess Game with Moved knight", game3);
 
             ChessGame game4 = new ChessGame();
             game4.setBoard(TestUtilities.loadBoard("""
@@ -56,7 +56,7 @@ public class ChessGameTests extends EqualsTestingUtility<ChessGame> {
                     | | | | |P| | | |
                     | | | |R| | | | |
                     """));
-            differentGames.put("Preset board", game4);
+            differentGames.put("Chess Game with Preset board", game4);
 
         } catch (InvalidMoveException e) {
             throw new RuntimeException("All moves in ChessGameTests are valid and should be allowed.", e);
