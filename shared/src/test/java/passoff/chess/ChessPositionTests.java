b85e50e878c823ae7ba71bbe07229a1be64818e3
Melvin Whitaker <awsome7mailman@gmail.com>
2025-10-29 13:45:26 -0600

    Modify ChessPositionTests to use updated EqualsTestingUtility

--- shared/src/test/java/passoff/chess/ChessPositionTests.java
+++ shared/src/test/java/passoff/chess/ChessPositionTests.java
@@ -2,29 +2,29 @@ package passoff.chess;
 
 import chess.ChessPosition;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
-public class ChessPositionTests {
+public class ChessPositionTests extends EqualsTestingUtility<ChessPosition> {
     public ChessPositionTests() {
-//        super("ChessPosition", "positions");
+        super("ChessPosition", "positions");
     }
 
-//    @Override
-//    protected ChessPosition buildOriginal() {
-//        return new ChessPosition(3, 7);
-//    }
-//
-//    @Override
-//    protected Collection<ChessPosition> buildAllDifferent() {
-//        return List.of(
-//                new ChessPosition(7, 3),
-//                new ChessPosition(6, 3),
-//                new ChessPosition(4, 3),
-//                new ChessPosition(3, 1),
-//                new ChessPosition(3, 2),
-//                new ChessPosition(3, 3)
-//        );
-//    }
+    @Override
+    protected Map.Entry<String, ChessPosition> buildOriginal() {
+        return Map.entry("Chess Position w/ row 3, column 7", new ChessPosition(3, 7));
+    }
+
+    @Override
+    protected Map<String, ChessPosition> buildAllDifferent() {
+        return Map.of(
+                "Chess Position w/ row 7, column 3", new ChessPosition(7, 3),
+                "Chess Position w/ row 6, column 3", new ChessPosition(6, 3),
+                "Chess Position w/ row 4, column 3", new ChessPosition(4, 3),
+                "Chess Position w/ row 3, column 1", new ChessPosition(3, 1),
+                "Chess Position w/ row 3, column 2", new ChessPosition(3, 2),
+                "Chess Position w/ row 3, column 3", new ChessPosition(3, 3)
+        );
+    }
 
 }
