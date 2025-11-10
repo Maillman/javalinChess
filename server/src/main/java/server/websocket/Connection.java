fbceba969b5db9f015d4df0a21f0ef3f64135b99
Melvin Whitaker <awsome7mailman@gmail.com>
2025-11-10 16:07:52 -0700

    Finalize WebSocketHandler TypeAdapter and created Connection class

---
+++ server/src/main/java/server/websocket/Connection.java
@@ -0,0 +1,21 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+
+public class Connection {
+    private final String authToken;
+    private final Session session;
+    public Connection(String authToken, Session session) {
+        this.authToken = authToken;
+        this.session = session;
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+}
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,21 +1,33 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.javalin.websocket.*;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.UserGameCommand;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
     @Override
     public void handleConnect(@NotNull WsConnectContext ctx) throws Exception {
-        ctx.enableAutomaticPings();
+        ctx.enableAutomaticPings(); //Need to enable automatic ping or else connection will close after 30 seconds
     }
 
     @Override
     public void handleMessage(@NotNull WsMessageContext ctx) throws Exception {
-
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(UserGameCommand.class, new UserGameCommand.UserGameCommandAdapter())
+                .create();
+        UserGameCommand userGameCommand = gson.fromJson(ctx.message(), UserGameCommand.class);
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> System.out.println("This is a connect command");
+            case MAKE_MOVE -> System.out.println("This is a make move command");
+            case LEAVE -> System.out.println("This is a leave command");
+            case RESIGN -> System.out.println("This is a resign command");
+        }
     }
 
     @Override
     public void handleClose(@NotNull WsCloseContext ctx) throws Exception {
-
+        // Nothing needs to be done here
     }
 }
